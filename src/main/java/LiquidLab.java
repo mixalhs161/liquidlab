adeb4388ada9f007d51c608c89907cf793e2ce72
χρηστος διακακης <t8230031@aueb.gr>
2024-11-26 13:20:44 +0000

    Commit of the main class

--- src/main/java/LiquidLab.java
+++ src/main/java/LiquidLab.java
@@ -1,3 +1,22 @@
 public class LiquidLab {
+    public static void liquidLab(String[] args) {
+        InsertValues iv1 = new InsertValues();
+        boolean success = false;
 
+        while (!success) {
+            try {
+                iv1.insert();
+                success = true; // Αν δεν πεταχτεί εξαίρεση, ολοκλήρωσε τον βρόχο
+            } catch (DrinkBaseException e) {
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("Please enter a valid base.");
+            } catch (FlavourException e) {
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("Please enter a valid flavor.");
+            } catch (AlcoholContentException e) {
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("Please enter a valid intensity.");
+            }
+        }
+    }
 }
