64312c478253425cefdf92e82e34427a1688e55f
Mixalhs Simitsis <t8230134@aueb.gr>
2025-01-11 15:28:30 +0200

    Update CocktailRecommenderTest.java

--- src/test/java/det/CocktailRecommenderTest.java
+++ src/test/java/det/CocktailRecommenderTest.java
@@ -1,4 +1,4 @@
-mport static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
